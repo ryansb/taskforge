b87a58a14321878b45f0f35d9dc45007c05d838a
Ryan S. Brown <sb@ryansb.com>
2014-11-16 19:48:03 -0500

    update license preamble

--- taskforge/commands/run.py
+++ taskforge/commands/run.py
@@ -1,7 +1,22 @@
-#!/bin/env python
 # -*- coding: utf8 -*-
-# Author: Ryan Brown <sb@ryansb.com>
-# License: Affero GPLv3
+"""
+Copyright 2014 Ryan Brown <sb@ryansb.com>
+
+This file is part of taskforge.
+
+taskforge is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+
+taskforge is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with taskforge.  If not, see <http://www.gnu.org/licenses/>.
+"""
 
 import logging
 
